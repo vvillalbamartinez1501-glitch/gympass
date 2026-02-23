966f0e26f4a387fa3404ba9eb387b58222dea18c
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-02-23 12:36:46 +0100

    Clase socio creada

--- src/Gimnasio.java
+++ src/Gimnasio.java
@@ -1,20 +1,2 @@
 public class Gimnasio {
-    private int numeroSocio;
-    private String nombre;
-
-    // getters
-    public int getNumeroSocio() {
-        return this.numeroSocio;
-    }
-
-    public String getNombre() {
-        return this.nombre;
-    }
-
-    @Override
-    public String toString(){
-        return
-        String.format("[%d] %s", this.numeroSocio, this.nombre);
-
-    }
 }
--- src/Sala.java
+++ src/Sala.java
@@ -1,2 +1,28 @@
 public class Sala {
+    private final int NUMERO_SOCIO;
+    private String nombre;
+
+    // constructor
+
+
+    public Sala(int numeroSocio, String nombre) {
+        this.NUMERO_SOCIO = numeroSocio;
+        this.nombre = nombre;
+    }
+
+    // getters
+    public int getNumeroSocio() {
+        return this.NUMERO_SOCIO;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    @Override
+    public String toString(){
+        return
+                String.format("[%d] %s", this.NUMERO_SOCIO, this.nombre);
+
+    }
 }
